d0bd1bc55a970b12fcaeccc91e1622b0f0244b66
ilyamore88 <themorozik@gmail.com>
2019-03-08 00:39:00 +0300

    style fix

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended',
-	'codex'
+    'codex'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -15,4 +15,4 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
