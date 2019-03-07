9722a851d5502c50f606fc4d09de201d42862c12
ilyamore88 <themorozik@gmail.com>
2019-03-08 00:27:25 +0300

    add codex eslint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    'eslint:recommended'
+    'eslint:recommended',
+	'codex'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
