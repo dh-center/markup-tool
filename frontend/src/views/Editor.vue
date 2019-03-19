50114d7e0d1b217c7ff5bc7cb31ef9edb78b5418
Nikita Melnikov <nikmel2803@gmail.com>
2019-03-19 19:20:58 +0300

    add base Editor view

---
+++ frontend/src/views/Editor.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="editor">
+    <div class="editor__settings">
+      settings bar
+    </div>
+    <div class="editor__content">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet autem blanditiis error est fuga numquam porro
+      quo repudiandae sunt! Impedit ipsam iure quaerat ut. Asperiores atque, beatae enim explicabo facilis inventore
+      ipsam nemo nisi officia quibusdam quis quo unde vel. Dolorem error ex harum hic modi molestiae neque perferendis,
+      possimus quibusdam. At consequatur cum fuga quos repellendus suscipit voluptatum! Deleniti nam repellendus
+      tempora. Animi error molestias provident sapiente similique totam veniam vitae, voluptatum! Ab, aliquam architecto
+      autem consequuntur corporis cum debitis eligendi fugiat id libero magni nulla odit possimus quas quod repellat,
+      sapiente similique. Ex id mollitia provident suscipit.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Editor'
+};
+</script>
+
+<style>
+  .editor {
+    display: flex;
+
+    &__settings{
+      border-right: 1px solid black;
+    }
+  }
+</style>
