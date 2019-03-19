21fac205a724e5a649a31318bf3317bd8020dd70
Nikita Melnikov <nikmel2803@gmail.com>
2019-03-19 21:13:16 +0300

    new interface

--- frontend/src/views/Editor.vue
+++ frontend/src/views/Editor.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="editor">
-    <div class="editor__settings">
-      settings bar
+    <div class="editor__toolbar">
+      <button>Новая сущность</button>
+      <button>Продолжить сущность</button>
+      <button>Нулевая анафора</button>
+      <button>Убрать выделение</button>
     </div>
-    <div class="editor__content">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet autem blanditiis error est fuga numquam porro
-      quo repudiandae sunt! Impedit ipsam iure quaerat ut. Asperiores atque, beatae enim explicabo facilis inventore
-      ipsam nemo nisi officia quibusdam quis quo unde vel. Dolorem error ex harum hic modi molestiae neque perferendis,
-      possimus quibusdam. At consequatur cum fuga quos repellendus suscipit voluptatum! Deleniti nam repellendus
-      tempora. Animi error molestias provident sapiente similique totam veniam vitae, voluptatum! Ab, aliquam architecto
-      autem consequuntur corporis cum debitis eligendi fugiat id libero magni nulla odit possimus quas quod repellat,
-      sapiente similique. Ex id mollitia provident suscipit.
+    <div class="editor__column-layout">
+      <div class="editor__content">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet autem blanditiis error est fuga numquam porro
+        quo repudiandae sunt! Impedit ipsam iure quaerat ut. Asperiores atque, beatae enim explicabo facilis inventore
+        ipsam nemo nisi officia quibusdam quis quo unde vel. Dolorem error ex harum hic modi molestiae neque
+        perferendis,
+        possimus quibusdam. At consequatur cum fuga quos repellendus suscipit voluptatum! Deleniti nam repellendus
+        tempora. Animi error molestias provident sapiente similique totam veniam vitae, voluptatum! Ab, aliquam
+        architecto
+        autem consequuntur corporis cum debitis eligendi fugiat id libero magni nulla odit possimus quas quod repellat,
+        sapiente similique. Ex id mollitia provident suscipit.
+      </div>
+      <div class="editor__entities-panel">
+        <h1>Панель сущностей</h1>
+      </div>
     </div>
   </div>
 </template>
@@ -23,10 +33,18 @@ export default {
 
 <style>
   .editor {
-    display: flex;
 
-    &__settings{
-      border-right: 1px solid black;
+    &__column-layout {
+      display: flex;
+    }
+
+    &__entities-panel {
+      border-left: 1px solid black;
+      flex: 25%;
+    }
+
+    &__content {
+      flex: 75%;
     }
   }
 </style>
