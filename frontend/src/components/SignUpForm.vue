80f459a31429068e779f23848504d95bc3c16947
SemBystrov <sembystrov14@gmail.com>
2019-03-21 13:14:09 +0300

    Добавлена страница регистрации. Сама форма вынесена как отдельный компонент. Добавил лого dhlab чтобы стимул был. Можете и удалить конечно :^( (Попытка номер 2)

---
+++ frontend/src/components/SignUpForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="SignUpForm">
+    <form @submit.prevent="signUp">
+      <h2>Регистрация</h2>
+      <label>Имя:</label><br>
+      <input required v-model="username" type="text" placeholder="Иванов Иван Иванович"/><br><br>
+      <label>E-mail:</label><br>
+      <input required v-model="mail" type="email" placeholder="ivanivanov@mail.ru"/><br><br>
+      <label>Пароль:</label><br>
+      <input required v-model="pass" type="password" /><br><br>
+      <hr>
+      <button type="submit">Отправить</button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+    export default{
+        name: "SignUpForm",
+        data () {
+          return {
+            username: "",
+            mail: "",
+            pass: ""
+          }
+        },
+        methods: {
+          signUp () {
+            alert(this.username + ", вы зарегистрировались!");
+            let xhr = new XMLHttpRequest();
+            xhr.open('post', 'localhost:3000/sign-up', false);
+            let body = "password=" + this.pass +"&email =" + this.mail;
+            xhr.send(body);
+            if (xhr.status != 200) {
+              alert( xhr.status + ': ' + xhr.statusText );
+            } else {
+              alert( xhr.responseText );
+            }
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/SignUp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="signup">
+     <img alt="DH logo" src="../assets/logodh.jpg" class="signup__dhlogo">
+     <SignUpForm />
+  </div>
+</template>
+
+<script>
+    import SignUpForm from '../components/SignUpForm.vue'
+    export default {
+        name: "SignUp",
+        components: {
+          SignUpForm
+        }
+    }
+</script>
+
+<style scoped>
+    .signup{
+      text-align: center;
+         &__dhlogo{
+            margin-top: 5vh;
+            height: 20vh;
+            min-height: 150px;
+         }
+
+    }
+</style>
