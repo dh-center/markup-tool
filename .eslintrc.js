288c7525198f2928cf1b53c6bbcf0cfdb141c0f8
Nikita Melnikov <nikmel2803@gmail.com>
2019-03-22 21:50:17 +0300

    reorganize eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'codex'
+  ],
+  rules:{
+    'prefer-const': ['error', {
+      'destructuring': 'all',
+      'ignoreReadBeforeAssign': false
+    }]
+  }
+};
