14eb7b08a7669f8e554cd909ada3595765caac8e
ilyamore88 <themorozik@gmail.com>
2019-03-25 01:14:44 +0300

    can select words with range

--- frontend/src/views/Editor.vue
+++ frontend/src/views/Editor.vue
@@ -11,8 +11,8 @@
     </div>
 
     <div class="editor__column-layout">
-      <div class="editor__content">
-        <span id="0" v-on:click="selectWord($event)">Lorem</span> <span id="1" v-on:click="selectWord($event)">ipsum</span> <span id="2" v-on:click="selectWord($event)">dolor</span> sit amet, consectetur adipisicing elit. Ab
+      <div class="editor__content" v-on:click="selectWord($event)" id="editor__content">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
         amet autem blanditiis error est fuga numquam porro
         quo repudiandae sunt! Impedit ipsam iure quaerat ut. Asperiores atque, beatae enim explicabo facilis inventore
         ipsam nemo nisi officia quibusdam quis quo unde vel. Dolorem error ex harum hic modi molestiae neque
@@ -49,10 +49,36 @@
         this.showModal = false;
       },
       selectWord(event) {
-        const wordId = event.currentTarget.id;
-        const clickedWord = document.getElementById(wordId);
+        const pos = document.caretRangeFromPoint(event.clientX, event.clientY);
+        const textNode = pos.startContainer.data;
+        const clickedChar = textNode[pos.startOffset];
 
-        clickedWord.classList.toggle('editor__content-selected');
+        console.log(clickedChar);
+        if (clickedChar === ' ') {
+          pos.setEnd(pos.endContainer, pos.endOffset + 1);
+          const highlightDiv = document.createElement('span');
+
+          highlightDiv.className = 'editor__content-selected';
+          pos.surroundContents(highlightDiv);
+        } else if (typeof clickedChar != 'undefined') {
+          let start = pos.startOffset;
+
+          while (start >= 1 && /\S/.test(textNode.charAt(start - 1))) {
+            --start;
+          }
+          pos.setStart(pos.startContainer, start);
+          let end = pos.endOffset;
+          const len = textNode.length;
+
+          while (end < len && /\S/.test(textNode.charAt(end))) {
+            ++end;
+          }
+          pos.setEnd(pos.endContainer, end);
+          const highlightDiv = document.createElement('span');
+
+          highlightDiv.className = 'editor__content-selected';
+          pos.surroundContents(highlightDiv);
+        }
       }
     },
     components: {
