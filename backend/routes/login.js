e905ebe375e5f123324fbaf6d763a2daf86db308
daniilpashov10 <daniilpashv@gmail.com>
2019-03-31 14:57:27 +0300

    add login.js

---
+++ backend/routes/login.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+router.get('/login', async (req, res) => {
+  let jwt = 'sadsad';
+
+  try {
+    User.find(function (err, users) {
+      if (err) return console.error(err);
+      if ((req.body.email === users.email) && (req.body.password === users.password)) jwt = users.generateJWT();
+    });
+
+    res.json({ jwt });
+    console.log('HELLO');
+  } catch (error) {
+    res.json({ error });
+  }
+});
+
+module.exports = router;
