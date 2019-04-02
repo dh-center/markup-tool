53b0455aa1570105256a9a42748652cabf3e7cb9
ilyamore88 <themorozik@gmail.com>
2019-04-02 21:33:52 +0300

    delete console.log

--- backend/routes/texts.js
+++ backend/routes/texts.js
@@ -6,10 +6,8 @@ router.get('/texts', async (req, res) => {
   try {
     const data = await Text.find();
 
-    console.log(data);
     res.json({ data });
   } catch (error) {
-    console.log(error);
     res.json({ error });
   }
 });
