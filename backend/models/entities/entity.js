fc70ff096d78aa0ad23358e3dd5ba1eb138dc35c
daniilpashov10 <daniilpashv@gmail.com>
2019-04-03 21:25:33 +0300

    add create entity

--- backend/models/entities/entity.js
+++ backend/models/entities/entity.js
@@ -5,4 +5,16 @@ const entitySchema = new Schema({
   name: String
 });
 
+/**
+ * Creates new entity in DB and returns him
+ * @param {String} name - name of entity
+ * @param {Function} [cb] - callback (if not specified, Promises are used)
+ * @returns {Promise} - created entity
+ */
+entitySchema.statics.create = async function (name, cb) {
+  const entity = new this({ name });
+
+  return entity.save(cb);
+};
+
 module.exports = mongoose.model('entities', entitySchema);
